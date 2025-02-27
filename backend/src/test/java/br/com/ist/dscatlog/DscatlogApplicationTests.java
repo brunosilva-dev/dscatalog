b9252363c23b4b7626b94c78c021fb5b850b75ee
brunosilva-dev <silva.dev@hotmail.com>
2025-02-26 22:40:22 -0300

    backend project created

---
+++ backend/src/test/java/br/com/ist/dscatlog/DscatlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ist.dscatlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
